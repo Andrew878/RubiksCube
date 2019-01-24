2fd93139252e72b6a212e8353c4b2efe69e85d99
al278 <al278@st-andrews.ac.uk>
2019-01-24 16:11:49 +0000

    Rotation of front added

--- Side.py
+++ Side.py
@@ -12,8 +12,11 @@ class Side(object):
         self.left_neighbour = None
         self.right_neighbour = None
         self.bottom_neighbour = None
-        #row = [initial_colour, initial_colour, initial_colour]
-        self.array_of_squares = [initial_colour, initial_colour, initial_colour], [initial_colour, initial_colour, initial_colour], [initial_colour, initial_colour, initial_colour]
+        # row = [initial_colour, initial_colour, initial_colour]
+        self.array_of_squares = [initial_colour, initial_colour, initial_colour], [initial_colour, initial_colour,
+                                                                                   initial_colour], [initial_colour,
+                                                                                                     initial_colour,
+                                                                                                     initial_colour]
 
     def setNeighbours(self, top_neighbour, left_neighbour, right_neighbour, bottom_neighbour):
         self.top_neighbour = top_neighbour
@@ -25,5 +28,14 @@ class Side(object):
         print("Side:", self.identifier)
         for row in self.array_of_squares[:]:
             print(row)
-
         print("")
+
+    def rotateFromFront(self, direction_clockwise=True):
+        c = list.copy(self.array_of_squares)
+
+        if (direction_clockwise):
+            self.array_of_squares = [[c[2][0], c[1][0], c[0][0]], [c[2][1], c[1][1], c[0][1]],
+                                     [c[2][2], c[1][2], c[0][2]]]
+        else:
+            self.array_of_squares = [[c[0][2], c[1][2], c[2][2]], [c[0][1], c[1][1], c[2][1]],
+                                     [c[0][0], c[1][0], c[2][0]]]

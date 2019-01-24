fa5269924a770ab9a94afc44bc36e7d66730c6c9
al278 <al278@st-andrews.ac.uk>
2019-01-24 14:48:17 +0000

    Side class written

--- Side.py
+++ Side.py
@@ -1,5 +1,29 @@
+import numpy as np
+
+
 class Side(object):
 
-    def __init__(self, identifier):
+    def __init__(self, identifier,
+                 initial_colour):  # , top_neighbour, left_neighbour, right_neighbour, bottom_neighbour):
         """Create a side of the rubix cube"""
-        self.
+        self.identifier = identifier
+        self.initial_colour = initial_colour
+        self.top_neighbour = None
+        self.left_neighbour = None
+        self.right_neighbour = None
+        self.bottom_neighbour = None
+        #row = [initial_colour, initial_colour, initial_colour]
+        self.array_of_squares = [initial_colour, initial_colour, initial_colour], [initial_colour, initial_colour, initial_colour], [initial_colour, initial_colour, initial_colour]
+
+    def setNeighbours(self, top_neighbour, left_neighbour, right_neighbour, bottom_neighbour):
+        self.top_neighbour = top_neighbour
+        self.left_neighbour = left_neighbour
+        self.right_neighbour = right_neighbour
+        self.bottom_neighbour = bottom_neighbour
+
+    def printSide(self):
+        print("Side:", self.identifier)
+        for row in self.array_of_squares[:]:
+            print(row)
+
+        print("")

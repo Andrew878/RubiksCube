27a220d2c08b31d9d863ba63e1eccef00e65aa6b
al278 <al278@st-andrews.ac.uk>
2019-01-24 16:19:53 +0000

    Rotation of front added with operation class

---
+++ CubeManipulator.py
@@ -0,0 +1,20 @@
+import Side as Side
+
+
+class CubeManipulator(object):
+
+    def __init__(self, all_sides):
+        self.all_sides = all_sides
+        self.front = all_sides[0]
+        self.top = all_sides[1]
+        self.right = all_sides[2]
+        self.left = all_sides[3]
+        self.bottom = all_sides[4]
+        self.back = all_sides[5]
+
+    def printCubeState(self):
+        for side in self.all_sides:
+            side.printSide()
+
+    def rotateSide(self, side_from_front_view, direction_clockwise=True):
+        side_from_front_view.rotateFromFront(direction_clockwise)
